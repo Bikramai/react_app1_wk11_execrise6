3cdf476ef511cdb5cd72a4792ef485747f884212
bikramia <bikram.rhce@gmail.com>
2023-12-25 09:54:53 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import ExpandableText from "./components/ExpandableText";
+
+
+function App() {
+
+  
+  
+  return (
+    <div>
+      <ExpandableText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Velit odit temporibus praesentium! Consectetur impedit quas, 
+        laborum provident iusto voluptate doloribus voluptas fuga maxime 
+        iste sapiente incidunt! Delectus consequatur nam consectetur error 
+        consequuntur necessitatibus iusto vitae, totam suscipit eveniet ad veniam 
+        placeat vero quia a molestias nobis! Unde minus maiores corrupti expedita 
+        iusto sunt possimus voluptatem cumque facilis ipsum quibusdam quia minima, 
+        accusantium atque dolores eveniet assumenda. Aliquid, impedit? Dolores enim 
+        est eum magni harum modi vitae voluptate? Dolor, adipisci laboriosam amet officiis 
+        sapiente ut voluptas, maiores culpa eveniet, cupiditate debitis maxime voluptate consequatur 
+        perspiciatis eum? Sed doloremque modi obcaecati quos unde! Laudantium exercitationem facilis excepturi possimus ab voluptatem incidunt delectus neque eligendi, at voluptate voluptates quos fugit cumque aliquam suscipit pariatur illo accusantium veniam sapiente corrupti aliquid. Veniam neque fugiat tempora consequuntur quae iure consequatur vero pariatur accusamus, voluptas reiciendis omnis. Voluptates culpa animi rerum aperiam ratione rem accusamus nihil velit aliquid maxime voluptatibus doloribus, quos recusandae consequuntur asperiores incidunt molestiae assumenda molestias ipsa dolores hic? Aperiam at animi optio minima, ab sed, vel, nostrum cum eveniet porro obcaecati sunt dolorum placeat magni error rem asperiores deleniti tempora labore! Blanditiis, animi. Laudantium enim accusamus aperiam? Quae, nesciunt cum? Doloribus, error.
+        
+        {/* Hello world! */}
+      </ExpandableText>
+    </div>
+    
+  )
+  };
+
+
+export default App;
